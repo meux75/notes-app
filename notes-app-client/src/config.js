d2d522a3fababeb2261c18f0118cb500eb731cee
meyux <meyuxx@gmail.com>
2019-03-26 23:35:25 +0800

    configure aws amplify

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "170394840186-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://5j1jpxasm4.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_b88Dqquce",
+    APP_CLIENT_ID: "1p64qmlmoc0ijroaksm08desad",
+    IDENTITY_POOL_ID: "us-east-1:24c40927-4fed-402e-9d04-8185c03b3a08"
+  }
+};
